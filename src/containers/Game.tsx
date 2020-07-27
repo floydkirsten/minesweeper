b10557829de42a6d5cfd7ede5d606b05692180cd
Kirsten Floyd <45470377+floydkirsten@users.noreply.github.com>
2020-07-27 11:34:11 -0400

    updates

--- src/containers/Game.tsx
+++ src/containers/Game.tsx
@@ -6,107 +6,68 @@ import Counter from '../components/Counter';
 
 export default function Game() {
 
-  const [bombsLeft, setBombsLeft] = useState(3);
+  const [bombsLeft, setBombsLeft] = useState(1);
+  const [size, setSize] = useState(4);
 
-  const [grid, setGrid] = useState([
-    [
-      {
-        bomb: false,
-        flagged: false, 
-        value: '',
-        adjacentBombs: 0,
-        opened: false
-      },
-      {
-        bomb: false,
-        flagged: false,
-        value: '',
-        adjacentBombs: 2,
-        opened: false
-      },
-      {
-        bomb: false,
-        flagged: false,
-        value: '',
-        adjacentBombs: 0,
-        opened: false
+  function makeInitGrid(size) {
+    var initGrid=new Array(size);
+    for(var i=0;i<size;i++) {
+      initGrid[i]=new Array(size);
+    }
+    for (var i=0;i<size;i++) {
+      for(var j=0;j<size;j++) {
+        initGrid[i][j] = {
+          bomb: false,
+          flagged: false, 
+          value: '',
+          adjacentBombs: 0,
+          opened: false
+        }
       }
-    ],
-    [
-      {
-        bomb: false,
-        flagged: false,
-        value: '',
-        adjacentBombs: 0,
-        opened: false
-      },
-      {
-        bomb: false,
-        flagged: false,
-        value: '',
-        adjacentBombs: 0,
-        opened: false
-      },
-      {
-        bomb: false,
-        flagged: false,
-        value: '',
-        adjacentBombs: 0,
-        opened: false
-      }],
-    [
-      {
-        bomb: false,
-        flagged: false,
-        value: '',
-        adjacentBombs: 0,
-        opened: false
-      },
-      { 
-        bomb: false,
-        flagged: false,
-        value: '',
-        adjacentBombs: 0,
-        opened: false
-      },
-      {
-        bomb: false,
-        flagged: false,
-        value: '',
-        adjacentBombs: 0,
-        opened: false
-      } 
-    ]
-  ]);
+    }
+    return initGrid;
+  }
 
+  const [grid, setGrid] = useState(makeInitGrid(size));
+    
   function startGame() {
-    //set bombs
-    //check adjacents
-    grid[0][0].bomb=true;
-    grid[2][1].bomb=true;
-    grid[2][0].bomb=true;
+    for (var i=0;i<bombsLeft;i++) {
+      var bomb=true;
+      while(bomb) {
+        var row = Math.floor(Math.random() * Math.floor(size));
+        var col = Math.floor(Math.random() * Math.floor(size));
+        if (grid[row][col].bomb==false) {
+          grid[row][col].bomb=true;
+          bomb = false;
+        }
+      }
+    }
 
     for (var row=0;row<grid.length;row++) {
       for (var col=0;col<grid[0].length;col++) {
         checkAdjacent(row, col);
       }
     }
-    
   }
 
   function handleClick(rowIndex: number, cellIndex: number, loop: boolean) {
-    grid[rowIndex][cellIndex].opened=true;
-    if(grid[rowIndex][cellIndex].bomb==true && !loop) {
+    
+    if (grid[rowIndex][cellIndex].opened==true) {
+
+    } else if(grid[rowIndex][cellIndex].bomb==true && !loop) {
+      grid[rowIndex][cellIndex].opened=true;
       explode();
     } else {
+      grid[rowIndex][cellIndex].opened=true;
       const newGrid = grid.map((row, index) => {
         if (index !== rowIndex) return row;
         return row.map((cell, index) => {
           if (index !== cellIndex) return cell;
           if (cell.adjacentBombs==0) {
-            cell.value="";
             if (rowIndex>0 && cellIndex>0) handleClick(rowIndex-1, cellIndex-1, true);
-            if (rowIndex>0) handleClick(rowIndex-1, cellIndex, true);
+            if (rowIndex>0) {
+              handleClick(rowIndex-1, cellIndex, true);
+            }
             if (cellIndex>0) handleClick(rowIndex, cellIndex-1, true);
             if (rowIndex<grid.length-1) handleClick(rowIndex+1, cellIndex, true);
             if (cellIndex<grid.length-1) handleClick(rowIndex, cellIndex+1, true);
@@ -122,8 +83,8 @@ export default function Game() {
       setGrid(newGrid);
       //update cell
       //call adjacents (not flagged or open)
+      //if(isGameOver()==true) alert("You won!");
     }
-    if(isGameOver()==true) alert("You won!");
   }
 
   function explode() {
@@ -142,8 +103,6 @@ export default function Game() {
     });
 
     setGrid(newGrid);
-
-    //alert("Game over!");
   }
 
   function checkAdjacent(row: number, cell: number) {
@@ -178,52 +137,17 @@ export default function Game() {
       }
       grid[row][cell].adjacentBombs=bombCount;
     }
-    
-    //if (grid[rowIndex+1][cellIndex+1].status="bomb") bombCount=bombCount+1;
-
-  /*   const checkGrid = grid.map((row, index) => {
-      if (index !== rowIndex+1 || index !== rowIndex-1 || index !== rowIndex) return row;
-      return row.map((cell, index) => {
-        if (index !== cellIndex+1 || index !== cellIndex-1 || index !== cellIndex) return cell;
-        console.log("mapping");
-        bombCount = bombCount+1;
-        console.log(bombCount);
-        return cell;
-      })
-    });
-
-    setGrid(checkGrid);
-    console.log(bombCount);
-    
-    const newGrid = grid.map((row, index) => {
-      if (index !== rowIndex) return row;
-      return row.map((cell, index) => {
-        if (index !== cellIndex) return cell;
-        console.log("mapping");
-        cell.value = bombCount.toString();
-        return cell;
-      })
-    });
-
-    grid.map((row, index) => {
-      if(index !== rowIndex+1 || index !== rowIndex-1 || index !== rowIndex) return row;
-      row.map((cell, index) => {
-        if(index !== cellIndex+1 || index !== cellIndex-1 || index !== cellIndex) return cell;
-        if (cell.bomb==true) bombCount=bombCount+1;
-        console.log("test");
-      })
-    })
-
-
-    setGrid(newGrid);
-    grid[rowIndex][cellIndex].value=bombCount.toString();
-    console.log(bombCount);
-    console.log(grid[rowIndex][cellIndex].value); */
+  
   }
 
   function isGameOver() {
-    //all flagged or open
-    if (bombsLeft==0) return true;
+    var gameOver=true;
+    grid.map((row, index) => {
+      row.map((cell, index) => {
+        if (!(cell.flagged==true || cell.opened==true)) gameOver=false; 
+      });
+    });
+    return gameOver;
   }
 
   function flagCell(rowIndex: number, cellIndex: number) {
@@ -232,32 +156,38 @@ export default function Game() {
       if (index !== rowIndex) return row;
       return row.map((cell, index) => {
         if (index !== cellIndex) return cell;
-        cell.flagged = !cell.flagged;
         if (cell.flagged==true) {
-          setBombsLeft(bombsLeft-1);
-          cell.value="FLAG"
-        } else {
           setBombsLeft(bombsLeft+1);
           cell.value=""
+          cell.flagged = !cell.flagged;
+        } else if (cell.opened==true) {
+        } else {
+          if (bombsLeft==0) {
+          } else {
+            setBombsLeft(bombsLeft-1);
+          cell.value="FLAG";
+          cell.flagged = !cell.flagged;
+          }
+          
         }
         return cell;
       });
     });
 
     setGrid(newGrid);
-    if (isGameOver()==true) alert("you won!");
+    //if (isGameOver()==true) alert("you won!");
+    console.log(bombsLeft);
   }
 
   function reset() {
     //start new game
-    setBombsLeft(3);
+    setBombsLeft(1);
     const newGrid = grid.map((row, index) => {
       return row.map((cell, index) => {
-        if (cell.flagged==true) {
-          cell.flagged=false;
-        }
+        cell.flagged=false;
         cell.opened=false;
         cell.value="";
+        cell.bomb=false;
         return cell;
       });
     });
