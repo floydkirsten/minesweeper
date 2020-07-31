000a071d792587c0ff1cf075fe66f01876f83c6f
Kirsten Floyd <45470377+floydkirsten@users.noreply.github.com>
2020-07-30 22:49:26 -0400

    more style changes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Button({onClick}: any) {
     return (
-        <button onClick={onClick} style={{width: 60, fontSize: 15}}> reset </button>
+        <button onClick={onClick} style={{width: 60, fontSize: 15, height: 50}}> reset </button>
     )
 }
--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -6,6 +6,12 @@ export default function Rules() {
             <div id="rules"> 
             to set flags: hold down shift and click
             </div>
+            <div id="link">
+            <a href={"https://github.com/floydkirsten"}> 
+            <img src={require('../images/github.png')} />
+            github.com/floydkirsten 
+            </a>
+            </div>
         </div>
     )
 }
